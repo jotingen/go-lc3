0dd93d15c43dde34eba33d110ba679fb38df1fea
jotingen <jotingen@gmail.com>
2018-06-05 22:56:43 -0400

    Move memory to be external the LC3. Implement ADD, AND, and BR instructions.

--- lc3/lc3.go
+++ lc3/lc3.go
@@ -10,28 +10,149 @@ type LC3 struct {
 
 	PC uint16
 
+	N bool
+	Z bool
+	P bool
+
 	Memory [65536]uint16
 }
 
-func (lc3 *LC3) Init() {
+type Request struct {
+	Vld     bool
+	RnW     bool
+	Address uint16
+	Data    uint16
+}
+
+func (lc3 *LC3) Init() uint16 {
 	for i := range lc3.Reg {
 		lc3.Reg[i] = uint16(rand.Intn(65536))
 	}
 	lc3.PC = 0x3000
+	fmt.Printf("Sending  PC: %04x\n", lc3.PC)
+	return lc3.PC
 }
 
-func (lc3 *LC3) Step() {
-	fmt.Println("vim-go")
+func (lc3 *LC3) Step(inst uint16, data uint16) (uint16, Request, error) {
+	fmt.Printf("Recieved Inst:x%04x Data:x%04x\n", inst, data)
+
+	//For now, if the instruction is unrecognized, panic
+	op := inst >> 12
+	switch op {
+
+	case 0x1: //ADD
+		dr := (inst & 0x0E00) >> 9
+		sr1 := (inst & 0x01C0) >> 6
+		bit5 := (inst & 0x0020) >> 5
+		if bit5 == 1 {
+			imm5 := inst & 0x001F
+			if (imm5&0x10)>>4 == 1 {
+				imm5 = imm5 | 0xFFE0
+			}
+			fmt.Printf("ADD R%d,R%d,#%d\n", dr, sr1, int16(imm5))
+			lc3.Reg[dr] = lc3.Reg[sr1] + imm5
+		} else {
+			sr2 := inst & 0x0007
+			fmt.Printf("ADD R%d,R%d,R%d\n", dr, sr1, sr2)
+			lc3.Reg[dr] = lc3.Reg[sr1] + lc3.Reg[sr2]
+		}
+		lc3.PC++
+		lc3.setCC(lc3.Reg[dr])
+
+	case 0x5: //AND
+		dr := (inst & 0x0E00) >> 9
+		sr1 := (inst & 0x01C0) >> 6
+		bit5 := (inst & 0x0020) >> 5
+		if bit5 == 1 {
+			imm5 := inst & 0x001F
+			if (imm5&0x10)>>4 == 1 {
+				imm5 = imm5 | 0xFFE0
+			}
+			fmt.Printf("AND R%d,R%d,#%d\n", dr, sr1, int16(imm5))
+			lc3.Reg[dr] = lc3.Reg[sr1] & imm5
+		} else {
+			sr2 := inst & 0x0007
+			fmt.Printf("AND R%d,R%d,R%d\n", dr, sr1, sr2)
+			lc3.Reg[dr] = lc3.Reg[sr1] & lc3.Reg[sr2]
+		}
+		lc3.PC++
+		lc3.setCC(lc3.Reg[dr])
+
+	case 0x0: //BR
+		n := (inst&0x0800)>>11 == 1
+		z := (inst&0x0400)>>10 == 1
+		p := (inst&0x0200)>>9 == 1
+		PCoffset9 := inst & 0x01FF
+		if (PCoffset9&0x100)>>8 == 1 {
+			PCoffset9 = PCoffset9 | 0xFE00
+		}
+		fmt.Print("BR")
+		if n {
+			fmt.Print("n")
+		}
+		if z {
+			fmt.Print("z")
+		}
+		if p {
+			fmt.Print("p")
+		}
+		fmt.Printf(" #%d\n", int16(PCoffset9))
+		if (n && lc3.N) || (z && lc3.Z) || (p && lc3.P) {
+			lc3.PC += PCoffset9
+		}
+
+	default:
+		return lc3.PC, Request{}, fmt.Errorf("Op not recognized: x%x", op)
+
+	}
+
+	fmt.Printf("Sending  PC:x%04x Req:%+v\n", lc3.PC, Request{})
+	return lc3.PC, Request{}, nil
 }
 
 func (lc3 LC3) String() (s string) {
 	s += "Reg:\n"
 	for i, r := range lc3.Reg {
-		s += fmt.Sprintf("%02d: %016b x%04x\n", i, r, r)
+		s += fmt.Sprintf("%02d: %016b x%04x %d\n", i, r, r, int16(r))
 	}
 	s += "\n"
 
 	s += fmt.Sprintf("PC: %016b x%04x d%d\n", lc3.PC, lc3.PC, lc3.PC)
 	s += "\n"
+
+	if lc3.P {
+		s += "P"
+	} else {
+		s += "p"
+	}
+	if lc3.Z {
+		s += "Z"
+	} else {
+		s += "z"
+	}
+	if lc3.N {
+		s += "N"
+	} else {
+		s += "n"
+	}
+	s += "\n"
 	return s
+}
+
+func isPositive(data uint16) bool {
+	return int16(data) > 0
+}
+
+func isZero(data uint16) bool {
+	return data == 0
+}
+
+func isNegative(data uint16) bool {
+	return int16(data) < 0
+}
+
+func (lc3 *LC3) setCC(data uint16) {
+	lc3.P = isPositive(data)
+	lc3.Z = isZero(data)
+	lc3.N = isNegative(data)
 }
--- main.go
+++ main.go
@@ -1,17 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 import (
 	"github.com/jotingen/go-lc3/lc3"
 )
 
+var (
+	memory [65536]uint16
+)
+
 func main() {
+	var err error
+
+	var r lc3.Request
+	var m uint16
+
 	fmt.Println("vim-go")
 	lc3 := lc3.LC3{}
-	lc3.Init()
+	pc := lc3.Init()
 
-	lc3.Step()
+	//Spoof some test instructions
+	memory[0x3000] = 0x103F //ADD R0,R0,#31
+	memory[0x3001] = 0x1001 //ADD R0,R0,R1
+	memory[0x3002] = 0x54A0 //AND R2,R2,#0
+	memory[0x3003] = 0x0E10 //BR (x3003 + x10)
+	memory[0x3013] = 0x56E0 //AND R3,R3,#0
 
+	cycles := 0
+	timeStart := time.Now()
 	fmt.Printf("%s\n", lc3)
+	for pc != 0x3014 {
+
+		pc, r, err = lc3.Step(memory[pc], m)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if r.Vld {
+			if r.RnW {
+				m = memory[r.Address]
+			} else {
+				memory[r.Address] = r.Data
+			}
+		}
+
+		fmt.Printf("%s\n", lc3)
+
+		cycles++
+
+	}
+	timeEnd := time.Now()
+
+	nanosecondsPerCycle := float64(timeEnd.Sub(timeStart)) / float64(cycles)
+	secondsPerCycle := float64(nanosecondsPerCycle) / 1000.0 / 1000.0 / 1000.0
+	hertz := 1 / secondsPerCycle
+	fmt.Printf("%dcycles/%s = %1.0fHz\n", cycles, timeEnd.Sub(timeStart), hertz)
+
 }
